b2461a3c36acc58b1c3bc9328bdf59516ba6d4fe
dan-wrigley-designamite <dan.wrigley@designamite.co.uk>
2024-11-14 16:25:48 +0000

    Bug fixes

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { supabase } from '@/lib/supabase';
 import Image from 'next/image';
 
